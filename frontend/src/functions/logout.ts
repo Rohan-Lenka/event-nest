93e557ba9f836a7a039fa23cdd9f6392f62823ec
Rohan Lenka <rohanlenka999@gmail.com>
2025-06-20 21:16:37 +0530

    changes in logout function in fe

--- frontend/src/functions/logout.ts
+++ frontend/src/functions/logout.ts
@@ -1,9 +1,8 @@
 import type { NavigateFunction } from "react-router-dom";
 
 function logout(navigate: NavigateFunction, person: "user" | "admin" | "moderator") {
-    console.log("clicked")
     localStorage.removeItem("token");
-    navigate(`/${person}/login`)
+    navigate(`/`)
 }
 
 export default logout
